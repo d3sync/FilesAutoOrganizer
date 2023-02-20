465333342cf696d7484fd0d6dd7117535b3a23b3
d3sync <alex@idle.gr>
2023-02-20 21:51:09 +0200

    Add project files.

---
+++ FilesAutoOrganizer/GroupByFiles.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+
+namespace FilesAutoOrganizer;
+
+public class GroupByModel
+{
+    public List<string>? EXT { get; init; }
+    public string? FolderDestination { get; init; }
+}
+public class GroupByFiles
+{
+    private static List<GroupByModel>? groupbylist = new List<GroupByModel>();
+    public GroupByFiles()
+    {
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "DOC", "DOCX", "DOTX", "DOT", "ODT", "PPT", "PPTX", "ODS", "ABW","CHM","DOCM","DOTM","EPUB",
+                    "MPP","ODF","OTT","XPS","POT","POTX","PPS","PPSX","PPTM","PPTX","PUB","RTF","SDD","SDW","SNP","VSD",
+                    "WPS","SXW","WRI","XLS","XLSX","SDC","SXS","XLSM","CHW","CHM"
+                },
+                FolderDestination = "Document's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "TXT","LOG"
+                },
+                FolderDestination = "Simple TXT's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "JS","HTML","HTM","C","CPP","JAVA","CSS","CS","PY","H","PHP","ASP","PERL","PL","CLASS","RS","LUA","SH","RB","TS","CC","HH","KT","KTS","GO","PS1"
+                    ,"PM","SCALA","SWIFT","PHTML","PHP5","M","PYC","TCL"
+                },
+                FolderDestination = "Source Code Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "JPG", "BMP", "JPEG", "GIF", "PNG", "ODT", "PSD", "EPS", "ICO","TIFF","PCX","RAW", "WEBP", "CPT", "PIC",
+                    "ICON","PSDX","TGA","EMF","WDP", "SVG","XCF","AI","CDR","INDD","CR2","CRW","NEF","PEF"
+                },
+                FolderDestination = "Image's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "WAV", "WMA", "MP3", "AAC", "OGG", "M4A", "REC","FLAC","MIDI","CDA", "ASF","M4P","M4B","M4A","PCM","ZAB", "AIFF","AMR"
+                },
+                FolderDestination = "Audio's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "264","3G2","3GP","ARF","ASF","ASX","AVI","BIK","DASH","DVR","FLV","H264","M2T","M2TS","M4V","MKV","MOD","MOV"
+                    ,"MP4","MPEG","MPG","MTS","OGV","PRPROJ","RMVB","SWF","VOB","WEBM","WLMP","WMV"
+                },
+                FolderDestination = "Video's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "EOT","OTF","TTC","TTF","WOFF"
+                },
+                FolderDestination = "Font's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "OVA","OVF","PVM","VDI","VHD","VHDX","VMDK","VMEM","VMX","VMWAREVM"
+                },
+                FolderDestination = "VM's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "BAT","EXE","COM","DLL","JAR","SYS","BIN","VBS","MSI","MSIX", "APPLICATION"
+                },
+                FolderDestination = "Executable's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "RAR","ZIP","7Z","GZ","TAR","TGZ","DEB","RPM","PKG","GZIP","CAB","ZIPX"
+                },
+                FolderDestination = "Compressed's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "000","ISO","IMG","CCD","CUE","DAA","DAO","NRG","TAO","UIF","VCD"
+                },
+                FolderDestination = "ISO's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "ACCDB","ACCDT","DB","SQL","SQLITE","MDF","IDX","FDB","CSV","DBF","GDB","MDB","SDF","WDB","XML","JSON"
+                },
+                FolderDestination = "Database's Folder"
+            });
+        groupbylist.Add(
+            new GroupByModel()
+            {
+                EXT = new List<string>()
+                {
+                    "LNK"
+                },
+                FolderDestination = "Link's Folder"
+            });
+    }
+    public void SerializeAndWriteToFile()
+    {
+        var filePath = global::System.IO.Path.Combine(global::System.IO.Directory.GetCurrentDirectory(), "appOrganizeSettings.json");
+        if (global::System.IO.File.Exists(filePath)) return;
+        var jsonString = JsonConvert.SerializeObject(groupbylist, Formatting.Indented);
+        global::System.IO.File.WriteAllText(filePath, jsonString);
+    }
+
+    public void ClearList()
+    {
+        groupbylist.Clear();
+    }
+    public void ReadFromFileAndDeserialize()
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "appOrganizeSettings.json");
+        if (!File.Exists(filePath)) return;
+        var jsonString = File.ReadAllText(filePath);
+        groupbylist = JsonConvert.DeserializeObject<List<GroupByModel>>(jsonString);
+    }
+    public string GetExtFolder(string ext)
+    {
+        var attempt = groupbylist!.FirstOrDefault(x => x.EXT!.Contains(ext));
+        return attempt?.FolderDestination ?? ext;
+    }
+}
---
+++ FilesAutoOrganizer/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace FilesAutoOrganizer;
+
+internal abstract class Program
+{
+    private static List<string> filesmd5list = new List<string>();
+    public static void Main(string[] args)
+    {
+        var downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
+        Array.Resize(ref args, args.Length + 3);
+        args[0] = "--calchash";
+        args[1] = "--organize";
+        args[2] = @"--path=C:\Users\d3sync\Documents\Downloads";
+        var gbf = new GroupByFiles();
+        if (args.Length < 1)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Usage: {Process.GetCurrentProcess().MainModule!.ModuleName} --organize --calchash [--path=C:\\Temp]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"If --path is not configured default path is: {downloadsPath}");
+            Environment.Exit(-1);
+        }
+        gbf.SerializeAndWriteToFile();
+        gbf.ClearList();
+        gbf.ReadFromFileAndDeserialize();
+
+
+        var argIndex = -1; // Initialize to -1, which means the argument wasn't found
+        for (var i = 0; i < args.Length; i++)
+        {
+            if (args[i].StartsWith("--path"))
+            {
+                argIndex = i;
+                break;
+            }
+        }
+        var pathing = args[argIndex];
+        var split = pathing.Split('=');
+        Console.WriteLine($"Path was set to {split[1]}");
+        if (!Directory.Exists(split[1]))
+        {
+            Console.WriteLine($"Invalid Path! {split[1]} doesn't exist!");
+            Environment.Exit(404);
+        }
+        else
+        {
+
+            downloadsPath = split[1];
+        }
+
+        if (Directory.Exists(downloadsPath))
+        {
+            Console.WriteLine($"Searching for files in {downloadsPath}");
+            var files = Directory.GetFiles(downloadsPath).ToList();
+            if (files.Count > 0)
+            {
+                try
+                {
+                    foreach (var file in files)
+                    {
+                        var item = new FileInfo(file);
+                        if (args.Contains("--calchash"))
+                        {
+                            CalculateFileHash(file);
+                        }
+
+                        if (args.Contains("--organize"))
+                        {
+                            var ext = item.Extension.Replace(".", "").ToUpper();
+                            var folderPath = Path.Combine(downloadsPath, gbf.GetExtFolder(ext));
+                            if (!Directory.Exists(folderPath))
+                            {
+                                Directory.CreateDirectory(folderPath);
+                                Console.WriteLine($"Creating folder: {folderPath}");
+                            }
+
+                            try
+                            {
+                                File.Move(file, folderPath + "\\" + item.Name);
+                                Console.WriteLine($"Moving {file} to {folderPath}\\{item.Name}");
+                            }
+                            catch (System.IO.IOException ioe)
+                            {
+                                Console.WriteLine(ioe.Message);
+                                Console.WriteLine(ioe.Data);
+                                File.Move(file, folderPath + $"\\({DateTime.Now:HHmm})-" + item.Name);
+                            }
+                        }
+                    }
+
+                    WriteToFileHashes();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"No {downloadsPath} folder available!");
+        }
+    }
+
+    private static void CalculateFileHash(string filePath)
+    {
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(filePath);
+        var hash = md5.ComputeHash(stream);
+        var hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+        filesmd5list.Add($"MD5 hash of {filePath}: {hashString}");
+    }
+
+    private static void WriteToFileHashes()
+    {
+        var folderPath = global::System.IO.Directory.GetCurrentDirectory();
+        var filePath = Path.Combine(folderPath, $"Organized-FilesMovedHashes-{DateTime.Now:ddMMyy-HHmm}.txt");
+        foreach (var item in filesmd5list)
+        {
+            File.AppendAllText(filePath, item + "\r\n");
+        }
+        Console.WriteLine($"Wrote All Md5 Hashes to {filePath}");
+    }
+}
